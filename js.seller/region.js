97bca6a794818413d9dd9dfe17fecdeb3db8f211
xuzunyuan <xuzunyuan@yiwang.com>
2015-03-09 02:08:04 +0800

    v

---
+++ js.seller/region.js
@@ -0,0 +1,100 @@
+var Region = [
+  {id:2,name:"华北",children:[
+                        	{"children":[{"id":2,"name":"北京"}],"id":1,"name":"北京"},
+                        	{"children":[{"id":100,"name":"天津"}],"id":99,"name":"天津"},
+                        	{"children":[{"id":335,"name":"太原"},{"id":346,"name":"大同"},{"id":358,"name":"阳泉"},{"id":364,"name":"长治"},{"id":378,"name":"晋城"},{"id":385,"name":"朔州"},{"id":392,"name":"晋中"},{"id":404,"name":"运城"},{"id":418,"name":"忻州"},{"id":433,"name":"临汾"},{"id":451,"name":"吕梁"}],"id":334,"name":"山西"},
+                        	{"children":[{"id":1549,"name":"济南"},{"id":1560,"name":"青岛"},{"id":1573,"name":"淄博"},{"id":1582,"name":"枣庄"},{"id":1589,"name":"东营"},{"id":1595,"name":"烟台"},{"id":1608,"name":"潍坊"},{"id":1621,"name":"济宁"},{"id":1634,"name":"泰安"},{"id":1641,"name":"威海"},{"id":1646,"name":"日照"},{"id":1651,"name":"莱芜"},{"id":1654,"name":"临沂"},{"id":1667,"name":"德州"},{"id":1679,"name":"聊城"},{"id":1688,"name":"滨州"},{"id":1696,"name":"菏泽"}],"id":1548,"name":"山东"},
+                        	{"children":[{"id":151,"name":"石家庄"},{"id":175,"name":"唐山"},{"id":190,"name":"秦皇岛"},{"id":198,"name":"邯郸"},{"id":218,"name":"邢台"},{"id":238,"name":"保定"},{"id":264,"name":"张家口"},{"id":282,"name":"承德"},{"id":294,"name":"沧州"},{"id":311,"name":"廊坊"},{"id":322,"name":"衡水"}],"id":150,"name":"河北"},
+                        	{"children":[{"id":466,"name":"呼和浩特"},{"id":476,"name":"包头"},{"id":486,"name":"乌海"},{"id":490,"name":"赤峰"},{"id":503,"name":"通辽"},{"id":512,"name":"鄂尔多斯"},{"id":521,"name":"呼伦贝尔"},{"id":535,"name":"巴彦淖尔"},{"id":543,"name":"乌兰察布"},{"id":555,"name":"兴安盟"},{"id":562,"name":"锡林郭勒盟"},{"id":575,"name":"阿拉善盟"}],"id":465,"name":"内蒙古"}
+                                                   ]},             
+ {id:1,name:"华东",children:[
+	{"children":[{"id":907,"name":"上海"}],"id":906,"name":"上海"},
+	{"children":[{"id":1000,"name":"南京"},{"id":1014,"name":"无锡"},{"id":1023,"name":"徐州"},{"id":1034,"name":"常州"},{"id":1042,"name":"苏州"},{"id":1054,"name":"南通"},{"id":1063,"name":"连云港"},{"id":1071,"name":"淮安"},{"id":1080,"name":"盐城"},{"id":1090,"name":"扬州"},{"id":1097,"name":"镇江"},{"id":1104,"name":"泰州"},{"id":1111,"name":"宿迁"}],"id":999,"name":"江苏"},
+	{"children":[{"id":1118,"name":"杭州"},{"id":1132,"name":"宁波"},{"id":1144,"name":"温州"},{"id":1156,"name":"嘉兴"},{"id":1164,"name":"湖州"},{"id":1170,"name":"绍兴"},{"id":1177,"name":"金华"},{"id":1187,"name":"衢州"},{"id":1194,"name":"舟山"},{"id":1199,"name":"台州"},{"id":1209,"name":"丽水"}],"id":1117,"name":"浙江"},
+	{"children":[{"id":1220,"name":"合肥"},{"id":1230,"name":"芜湖"},{"id":1239,"name":"蚌埠"},{"id":1247,"name":"淮南"},{"id":1254,"name":" 马鞍山"},{"id":1261,"name":"淮北"},{"id":1266,"name":"铜陵"},{"id":1271,"name":"安庆"},{"id":1283,"name":"黄山"},{"id":1291,"name":"滁州"},{"id":1300,"name":"阜阳"},{"id":1309,"name":"宿州"},{"id":1315,"name":"六安"},{"id":1323,"name":"亳州"},{"id":1328,"name":"池州"},{"id":1333,"name":"宣城"}],"id":1219,"name":"安徽"},
+	{"children":[{"id":1437,"name":"南昌"},{"id":1447,"name":"景德镇"},{"id":1452,"name":"萍乡"},{"id":1458,"name":"九江"},{"id":1472,"name":"新余"},{"id":1475,"name":"鹰潭"},{"id":1479,"name":"赣州"},{"id":1498,"name":"吉安"},{"id":1512,"name":"宜春"},{"id":1523,"name":"抚州"},{"id":1535,"name":"上饶"}],"id":1436,"name":"江西"}
+                           ]},  
+ {id:3,name:"华中",children:[
+	{"children":[{"id":2088,"name":"长沙"},{"id":2098,"name":"株洲"},{"id":2108,"name":"湘潭"},{"id":2114,"name":"衡阳"},{"id":2127,"name":"邵阳"},{"id":2140,"name":"岳阳"},{"id":2150,"name":"常德"},{"id":2160,"name":"张家界"},{"id":2165,"name":"益阳"},{"id":2172,"name":"郴州"},{"id":2184,"name":"永州"},{"id":2196,"name":"怀化"},{"id":2209,"name":"娄底"},{"id":2215,"name":"湘西"}],"id":2087,"name":"湖南"},
+	{"children":[{"id":1885,"name":"武汉"},{"id":1899,"name":"黄石"},{"id":1906,"name":"十堰"},{"id":1915,"name":"宜昌"},{"id":1929,"name":"襄阳"},{"id":1939,"name":"鄂州"},{"id":1943,"name":"荆门"},{"id":1949,"name":"孝感"},{"id":1957,"name":"荆州"},{"id":1966,"name":"黄冈"},{"id":1977,"name":"咸宁"},{"id":1984,"name":"随州"},{"id":1988,"name":"恩施"},{"id":1997,"name":"仙桃"},{"id":2023,"name":"潜江"},{"id":2049,"name":"天门"},{"id":2078,"name":"神农架"}],"id":1884,"name":"湖北"},
+	{"children":[{"id":1707,"name":"郑州"},{"id":1720,"name":"开封"},{"id":1731,"name":"洛阳"},{"id":1747,"name":"平顶山"},{"id":1758,"name":" 安阳"},{"id":1768,"name":"鹤壁"},{"id":1774,"name":"新乡"},{"id":1787,"name":" 焦作"},{"id":1798,"name":"濮阳"},{"id":1805,"name":"许昌"},{"id":1812,"name":" 漯河"},{"id":1818,"name":"三门峡"},{"id":1825,"name":"南阳"},{"id":1839,"name":"商丘"},{"id":1849,"name":"信阳"},{"id":1860,"name":"周口"},{"id":1871,"name":"驻马店"},{"id":1882,"name":"济源"}],"id":1706,"name":"河南"}
+                           ]}, 
+ {id:4,name:"华南",children:[
+	{"children":[{"id":2225,"name":"广州"},{"id":2238,"name":"韶关"},{"id":2249,"name":"深圳"},{"id":2256,"name":"珠海"},{"id":2260,"name":"汕头"},{"id":2268,"name":"佛山"},{"id":2274,"name":"江门"},{"id":2282,"name":"湛江"},{"id":2292,"name":"茂名"},{"id":2299,"name":"肇庆"},{"id":2308,"name":"惠州"},{"id":2314,"name":"梅州"},{"id":2323,"name":"汕尾"},{"id":2328,"name":"河源"},{"id":2335,"name":"阳江"},{"id":2340,"name":"清远"},{"id":2349,"name":"中山"},{"id":2375,"name":"东莞"},{"id":2409,"name":"潮州"},{"id":2413,"name":"揭阳"},{"id":2419,"name":"云浮"}],"id":2224,"name":"广东"},
+	{"children":[{"id":2426,"name":"南宁"},{"id":2439,"name":"柳州"},{"id":2450,"name":"桂林"},{"id":2468,"name":"梧州"},{"id":2476,"name":"北海"},{"id":2481,"name":"防城港"},{"id":2486,"name":"钦州"},{"id":2491,"name":"贵港"},{"id":2497,"name":"玉林"},{"id":2504,"name":"百色"},{"id":2517,"name":"贺州"},{"id":2522,"name":"河池"},{"id":2534,"name":"来宾"},{"id":2541,"name":"崇左"}],"id":2425,"name":"广西"},
+	{"children":[{"id":1342,"name":"福州"},{"id":1356,"name":"厦门"},{"id":1363,"name":"莆田"},{"id":1369,"name":"三明"},{"id":1382,"name":"泉州"},{"id":1395,"name":"漳州"},{"id":1407,"name":"南平"},{"id":1418,"name":"龙岩"},{"id":1426,"name":"宁德"}],"id":1341,"name":"福建"},
+	{"children":[{"id":2550,"name":"海口"},{"id":2555,"name":"三亚"},{"id":2569,"name":"五指山"},{"id":2578,"name":"琼海"},{"id":2596,"name":"儋州"},{"id":2627,"name":"文昌"},{"id":2657,"name":"万宁"},{"id":2674,"name":"东方"},{"id":2687,"name":"定安县"},{"id":2702,"name":"屯昌县"},{"id":2714,"name":"澄迈县"},{"id":2730,"name":"临高县"},{"id":2745,"name":"白沙县"},{"id":2761,"name":"昌江县"},{"id":2774,"name":"乐东县"},{"id":2792,"name":"陵水县"},{"id":2808,"name":"保亭县"},{"id":2823,"name":"琼中县"},{"id":2840,"name":"西沙群岛"},{"id":2841,"name":"南沙群岛"},{"id":2842,"name":"中沙群岛"}],"id":2549,"name":"海南"}
+                           ]}, 
+ {id:5,name:"东北",children:[
+	{"children":[{"id":580,"name":"沈阳"},{"id":594,"name":"大连"},{"id":605,"name":"鞍山"},{"id":613,"name":"抚顺"},{"id":621,"name":"本溪"},{"id":628,"name":"丹东"},{"id":635,"name":"锦州"},{"id":643,"name":"营口"},{"id":650,"name":"阜新"},{"id":658,"name":"辽阳"},{"id":666,"name":"盘锦"},{"id":671,"name":"铁岭"},{"id":679,"name":"朝阳"},{"id":687,"name":"葫芦岛"}],"id":579,"name":"辽宁"},
+	{"children":[{"id":695,"name":"长春"},{"id":706,"name":"吉林"},{"id":716,"name":"四平"},{"id":723,"name":"辽源"},{"id":728,"name":"通化"},{"id":736,"name":"白山"},{"id":743,"name":"松原"},{"id":749,"name":"白城"},{"id":755,"name":"延边"}],"id":694,"name":"吉林"},
+	{"children":[{"id":765,"name":"哈尔滨"},{"id":784,"name":"齐齐哈尔"},{"id":801,"name":"鸡西"},{"id":811,"name":"鹤岗"},{"id":820,"name":"双鸭山"},{"id":829,"name":"大庆"},{"id":839,"name":"伊春"},{"id":857,"name":"佳木斯"},{"id":868,"name":"七台河"},{"id":873,"name":"牡丹江"},{"id":884,"name":"黑河"},{"id":891,"name":"绥化"},{"id":902,"name":"大兴安岭"}],"id":764,"name":"黑龙江"}
+                           ]}, 
+ {id:6,name:"西北",children:[
+	{"children":[{"id":4435,"name":"西安"},{"id":4449,"name":"铜川"},{"id":4454,"name":"宝鸡"},{"id":4467,"name":"咸阳"},{"id":4482,"name":"渭南"},{"id":4494,"name":"延安"},{"id":4508,"name":"汉中"},{"id":4520,"name":"榆林"},{"id":4533,"name":"安康"},{"id":4544,"name":"商洛"}],"id":4434,"name":"陕西"},
+	{"children":[{"id":4737,"name":"乌鲁木齐"},{"id":4746,"name":"克拉玛依"},{"id":4751,"name":"吐鲁番"},{"id":4755,"name":"哈密"},{"id":4759,"name":"昌吉"},{"id":4767,"name":"博尔塔拉"},{"id":4771,"name":"巴音郭楞"},{"id":4781,"name":" 阿克苏"},{"id":4791,"name":"克孜勒苏"},{"id":4796,"name":"喀什"},{"id":4809,"name":"和田"},{"id":4818,"name":"伊犁"},{"id":4829,"name":"塔城"},{"id":4837,"name":"阿勒泰"},{"id":4845,"name":"石河子"},{"id":4847,"name":"阿拉尔"},{"id":4849,"name":"图木舒克"},{"id":4851,"name":"五家渠"}],"id":4736,"name":"新疆"},
+	{"children":[{"id":4553,"name":"兰州"},{"id":4562,"name":"嘉峪关"},{"id":4566,"name":"金昌"},{"id":4569,"name":"白银"},{"id":4575,"name":"天水"},{"id":4583,"name":"武威"},{"id":4588,"name":"张掖"},{"id":4595,"name":"平凉"},{"id":4603,"name":"酒泉"},{"id":4611,"name":"庆阳"},{"id":4620,"name":"定西"},{"id":4628,"name":"陇南"},{"id":4638,"name":"临夏州"},{"id":4647,"name":"甘南州"}],"id":4552,"name":"甘肃"},
+	{"children":[{"id":4709,"name":"银川"},{"id":4716,"name":"石嘴山"},{"id":4720,"name":"吴忠"},{"id":4726,"name":"固原"},{"id":4732,"name":"中卫"}],"id":4708,"name":"宁夏"},
+	{"children":[{"id":4657,"name":"西宁"},{"id":4665,"name":"海东"},{"id":4672,"name":"海北州"},{"id":4677,"name":"黄南"},{"id":4682,"name":"海南"},{"id":4688,"name":"果洛"},{"id":4695,"name":"玉树"},{"id":4702,"name":"海西"}],"id":4656,"name":"青海"}
+                           ]}, 
+ {id:7,name:"西南",children:[
+	{"children":[{"id":2844,"name":"重庆"}],"id":2843,"name":"重庆"},
+	{"children":[{"id":4208,"name":"昆明"},{"id":4223,"name":"曲靖"},{"id":4233,"name":"玉溪"},{"id":4243,"name":"保山"},{"id":4249,"name":"昭通"},{"id":4261,"name":"丽江"},{"id":4267,"name":"普洱"},{"id":4278,"name":"临沧"},{"id":4287,"name":"楚雄"},{"id":4298,"name":"红河"},{"id":4312,"name":"文山"},{"id":4321,"name":"西双版纳"},{"id":4325,"name":"大理"},{"id":4338,"name":"德宏"},{"id":4344,"name":"怒江"},{"id":4349,"name":"迪庆"}],"id":4207,"name":"云南"},
+	{"children":[{"id":4110,"name":"贵阳"},{"id":4121,"name":"六盘水"},{"id":4126,"name":"遵义"},{"id":4141,"name":"安顺"},{"id":4148,"name":"毕节"},{"id":4157,"name":"铜仁"},{"id":4168,"name":"黔西南"},{"id":4177,"name":"黔东南"},{"id":4194,"name":"黔南"}],"id":4109,"name":"贵州"},
+	{"children":[{"id":4354,"name":"拉萨"},{"id":4363,"name":"昌都"},{"id":4375,"name":"山南"},{"id":4388,"name":"日喀则"},{"id":4407,"name":"那曲"},{"id":4418,"name":"阿里"},{"id":4426,"name":"林芝"}],"id":4353,"name":"西藏"},
+	{"children":[{"id":3907,"name":"成都"},{"id":3927,"name":"自贡"},{"id":3934,"name":"攀枝花"},{"id":3940,"name":"泸州"},{"id":3948,"name":"德阳"},{"id":3955,"name":"绵阳"},{"id":3965,"name":"广元"},{"id":3973,"name":"遂宁"},{"id":3979,"name":"内江"},{"id":3985,"name":"乐山"},{"id":3997,"name":"南充"},{"id":4007,"name":"眉山"},{"id":4014,"name":"宜宾"},{"id":4025,"name":"广安"},{"id":4031,"name":"达州"},{"id":4039,"name":"雅安"},{"id":4048,"name":"巴中"},{"id":4053,"name":"资阳"},{"id":4058,"name":"阿坝"},{"id":4072,"name":"甘孜"},{"id":4091,"name":"凉山"}],"id":3906,"name":"四川"}
+                           ]}              
+];
+
+function findProvince(id) {
+	for(var i = 0; i < Region.length; i++) {
+		var area = Region[i];
+		
+		for(var j = 0; j < area.children.length; j++) {
+			var province = area.children[j];
+			
+			if(province.id == id) return province;
+		}
+	}
+	
+	return null;
+}
+
+function renderCity(province, city) {
+	province.find('option:gt(0)').remove();
+	
+	$(Region).each(function(){
+		$(this.children).each(function(){
+			province.append('<option value="' +  this.id + '">' + this.name + '</option>');
+		});
+	});
+		
+	if(city) {
+		province.change(function(){
+			city.find('option:gt(0)').remove();
+			
+			var provinceVal = province.val();
+			if(provinceVal) {
+				var destProvince = findProvince(provinceVal);
+				
+				if(destProvince) {
+					$(destProvince.children).each(function(){
+						city.append('<option value="' +  this.id + '">' + this.name + '</option>');
+					});
+				}
+			}
+		});
+	}	
+	
+	var initProvince = province.attr('initValue');
+	if(initProvince) {
+		province.val(initProvince);
+		province.trigger('change');
+		
+		var initCity = city.attr('initValue');
+		
+		if(initCity) {
+			city.val(initCity);
+		}
+	}
+}
